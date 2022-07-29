9fc9f0c7dc159c910fea42e2119cb72da7555605
ultraman_nexus <tay.tang@outlook.com>
2022-07-29 23:09:10 +1000

    M2 ez

---
+++ Taylor_leetcode_prac/M2.java
@@ -0,0 +1,57 @@
+package Taylor_leetcode_prac;
+
+/*
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Thoughts:
+
+    a dummy way of doing this is to convert each listnode into int, add them, then convert them into list node.
+    Low efficient, not smart, extra works
+
+    a proper way to do this, is to add numbers directly on listnode, as when we go throught the listnode, the result should be ready
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class M2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+
+            curr = curr.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
