63e370d40753033c0d64b0a7ed3405c9fa7406f9
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-11-27 14:10:21 +1100

    update test case

--- Yi-Cheng_leetcode_prac/M931_Minimum_Falling_Path_Sum/Minimum_Falling_Path_Sum.py
+++ Yi-Cheng_leetcode_prac/M931_Minimum_Falling_Path_Sum/Minimum_Falling_Path_Sum.py
@@ -3,12 +3,18 @@ class Solution:
         pass
 
 
-def test():
-    matrix = [[2,1,3],[6,5,4],[7,8,9]]
+def test(matrix):
     s = Solution()
     res = s.minFallingPathSum(matrix)
     return res
 
 
 if __name__ == "__main__":
-    assert test() == 13
+
+    # Test case 1
+    matrix = [[2,1,3],[6,5,4],[7,8,9]]
+    assert test(matrix) == 13
+
+    # Test case 2
+    matrix = [[-19,57],[-40,-5]]
+    assert test(matrix) == -59
