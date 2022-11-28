d4265ba31836a0989279c6521511b360dfa0dab0
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-11-28 15:40:14 +1100

    M931

--- Yi-Cheng_leetcode_prac/M931_Minimum_Falling_Path_Sum/Minimum_Falling_Path_Sum.py
+++ Yi-Cheng_leetcode_prac/M931_Minimum_Falling_Path_Sum/Minimum_Falling_Path_Sum.py
@@ -1,6 +1,33 @@
 class Solution:
     def minFallingPathSum(self, matrix):
-        pass
+        # Get the size n
+        n = len(matrix)
+        # Copy input to update cell values throughtout the process
+        grid = matrix
+
+        # Start from second row
+        for i in range(1, n):
+            for j in range(n):
+
+                # Cost from top left(i-1, j-1), check for boundaries
+                if j-1 < 0:
+                    left = float('inf')
+                else:
+                    left = grid[i-1][j-1]
+
+                # Cost from top middle(i-1, j), don't need to check boundaries
+                middle = grid[i-1][j]
+
+                # Cost from top right(i-1, j+1), check for boundaries
+                if j+1 > n-1:
+                    right = float('inf')
+                else:
+                    right = grid[i-1][j+1]
+
+                # Choose the min cost path and update current cell value
+                grid[i][j] = grid[i][j] + min(left, middle, right)
+
+        return min(grid[n-1])
 
 
 def test(matrix):
